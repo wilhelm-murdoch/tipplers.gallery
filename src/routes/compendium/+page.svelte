51081a06eb482620796d554e41a155e0f7a16d8b
Wilhelm Murdoch <wilhelm@devilmayco.de>
2023-01-23 01:24:42 +1000

    add seo

--- src/routes/compendium/+page.svelte
+++ src/routes/compendium/+page.svelte
@@ -3,6 +3,7 @@
 	import Footer from '$components/Footer/Footer.svelte';
 	import Header from '$components/Header/Header.svelte';
 	import { onMount } from 'svelte';
+	import SvelteSeo from 'svelte-seo';
 	import { fade } from 'svelte/transition';
 
 	let data: any;
@@ -28,51 +29,75 @@
 
 		charsNav = charsNav;
 	});
+
+	const twitterMeta = {
+		site: '@wilhelm',
+		title: "Tippler's Gallery ~ Compendium",
+		description: 'Quickly search & browse through thousands of cocktail recipes gathered from around the web.',
+		image: 'https://tipplers.gallery/images/og-cover.png',
+		imageAlt: "Tippler's Gallery"
+	};
+
+	const openGraphMeta = {
+		title: "Tippler's Gallery ~ Compendium",
+		description: 'Quickly search & browse through thousands of cocktail recipes gathered from around the web.',
+		type: 'article',
+		url: 'https://tipplers.gallery/compendium',
+		images: [
+			{
+				url: 'https://tipplers.gallery/images/og-cover.png',
+				alt: "Tippler's Gallery"
+			}
+		]
+	};
 </script>
 
-<Header />
+<div class="px-2">
+	<Header />
+</div>
 
 <div class="max-w-7xl mx-auto text-center py-10">
 	<h2 class="text-6xl font-thin italic">The Compendium</h2>
 </div>
-{#if charsNav.length}
-	<div class="bg-white border-y">
-		<div class="max-w-7xl mx-auto">
-			<nav class="flex justify-between py-5 bg-white overflow-x-auto">
-				{#each charsNav as item, i}
-					{#if item.cocktails.length}
-						<a class="rounded-md px-4 py-2.5 hover:bg-tipplers-primary hover:text-white" href="#{item.char}" title={item.char}>
-							{item.char}
-						</a>
-					{:else}
-						<span class="px-4 py-2.5 text-tipplers-secondary">{item.char}</span>
-					{/if}
-				{/each}
-			</nav>
-		</div>
-	</div>
 
-	<div class="max-w-7xl mx-auto px-2">
-		{#each charsNav as item, i}
-			{#if item.cocktails.length}
-				<section>
-					<h3 id={item.char} class="py-8 text-2xl font-semibold font-serif border-b border-slate-50 bg-tipplers-lightest/25">
-						<span class="underline">{item.char}</span>
-						<span class="bg-white px-2 py-1 rounded shadow-sm text-xs font-normal font-sans align-top">
-							<strong>{item.cocktails.length}</strong>
-						</span>
-					</h3>
-					<div class="grid grid-cols-2 gap-6 md:grid-cols-4 lg:grid-cols-6">
-						{#each item.cocktails as cocktail, i}
-							<div in:fade={{ delay: i * 100 }}>
-								<CardSlim name={cocktail.name} src={cocktail.images[0].relative_path} from={cocktail.meta.sourced_from} attribution={cocktail.images[0].attribution} url={cocktail.meta.source_url} />
-							</div>
-						{/each}
-					</div>
-				</section>
-			{/if}
-		{/each}
+<div class="bg-white border-y">
+	<div class="max-w-7xl mx-auto">
+		<nav class="flex justify-between py-5 bg-white overflow-x-auto">
+			{#each charsNav as item, i}
+				{#if item.cocktails.length}
+					<a class="rounded-md px-4 py-2.5 hover:bg-tipplers-primary hover:text-white" href="#{item.char}" title={item.char}>
+						{item.char}
+					</a>
+				{:else}
+					<span class="px-4 py-2.5 text-tipplers-secondary">{item.char}</span>
+				{/if}
+			{/each}
+		</nav>
 	</div>
-{/if}
+</div>
+
+<div class="max-w-7xl mx-auto px-2">
+	{#each charsNav as item, i}
+		{#if item.cocktails.length}
+			<section>
+				<h3 id={item.char} class="py-8 text-2xl font-semibold font-serif border-b border-slate-50 bg-tipplers-lightest/25">
+					<span class="underline">{item.char}</span>
+					<span class="bg-white px-2 py-1 rounded shadow-sm text-xs font-normal font-sans align-top">
+						<strong>{item.cocktails.length}</strong>
+					</span>
+				</h3>
+				<div class="grid grid-cols-2 gap-6 md:grid-cols-4 lg:grid-cols-6">
+					{#each item.cocktails as cocktail, i}
+						<div in:fade={{ delay: i * 100 }}>
+							<CardSlim name={cocktail.name} src={cocktail.images[0].relative_path} from={cocktail.meta.sourced_from} attribution={cocktail.images[0].attribution} url={cocktail.meta.source_url} />
+						</div>
+					{/each}
+				</div>
+			</section>
+		{/if}
+	{/each}
+</div>
 
 <Footer />
+
+<SvelteSeo title="Tippler's Gallery ~ Compendium" description="Quickly search & browse through thousands of cocktail recipes gathered from around the web." canonical="https://tipplers.gallery/compendium" twitter={twitterMeta} openGraph={openGraphMeta} />
