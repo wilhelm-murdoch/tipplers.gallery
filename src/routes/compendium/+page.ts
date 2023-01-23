03508a20225c1ed3f5c0483b129c7a46922c7f1f
Wilhelm Murdoch <wilhelm@devilmayco.de>
2023-01-24 03:30:53 +1000

    testing something out

--- src/routes/compendium/+page.ts
+++ src/routes/compendium/+page.ts
@@ -1,6 +1,6 @@
 import { getBaseUrl } from "$lib/utils/urls";
 
-export const load = async ({ fetch }) => {
+export const load = async () => {
   const response = await fetch(getBaseUrl() + `/api/cocktails-thebar.json`);
 
   let cocktails = await response.json();
--- src/routes/compendium/tags/+page.ts
+++ src/routes/compendium/tags/+page.ts
@@ -1,6 +1,6 @@
 import { getBaseUrl } from "$lib/utils/urls";
 
-export const load = async ({ fetch }) => {
+export const load = async () => {
   const response = await fetch(getBaseUrl() + `/api/tags.json`);
 
   let tags = await response.json();
